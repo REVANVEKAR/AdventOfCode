c378063b1b493533b57d002daeee930890f8ea4b
Revanth N Anvekar <revanthanvekar11@gmail.com>
2023-12-02 00:27:45 +0530

    ok dosto

--- com/learning/Day1.java
+++ com/learning/Day1.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Main {
+public class Day1 {
 
     public static void main(String[] args) {
 	// write your coe here
